93fd988bb15760f3f00bbb7aba4ba4ab716ce071
Samuel N <samuelnegash56@gmail.com>
2022-06-02 22:05:02 +0300

    added description for number 4 and number 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+* main - prints the size of various types
+* Return: 0 if exited properly, non-zero otherwise
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+* main - prints the size of various types
+* Return: 0 if exited properly, non-zero otherwise
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
