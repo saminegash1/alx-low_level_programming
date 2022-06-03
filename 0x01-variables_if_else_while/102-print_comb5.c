56995dc9d3521a0e62f029612d07242bb2cf0ea9
Samuel N <samuelnegash56@gmail.com>
2022-06-03 23:16:41 +0300

    done with the task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,32 +7,27 @@
  */
 int main(void)
 {
-    int ch;
-    int n;
-    int k;
-    int m;
-    for (ch = 48; ch <= 57; ch++)
-    {
-        for (n = 48; n <= 57; n++)
-        {
-            for (k = 48; k <= 57; k++)
-            {
-                for (m = 48; m <= 57; m++)
-                {
-                    putchar(ch);
-                    putchar(n);
-                    putchar(32)
-                        putchar() if (ch != 57 || m != 57)
-                    {
-                        putchar(44);
-                        putchar(32);
-                    }
-                }
-            }
-        }
-    }
+int i, j;
 
-    putchar(10); /* this is an ascii code for new line*/
-
-    return (0);
+for (i = 0; i < 100; i++)
+{
+for (j = 0; j < 100; j++)
+{
+if (i < j)
+{
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
+putchar(' ');
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
