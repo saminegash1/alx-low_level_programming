8aa7de0418d526595b22638999e20d9ec3297a8f
Samuel N <samuelnegash56@gmail.com>
2022-06-07 21:40:45 +0300

    printing to 98 from both direction

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.c"
+
+/**
+* print_to_98 - prints to 98
+* @n: the starting point
+*/
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			if (n == 98)
+				printf("%d\n", n);
+			else
+				printf("%d, ", n);
+			n = n++;
+		}
+	} else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			if (n == 98)
+				printf("%d\n", n);
+			else
+				printf("%d, ", n);
+		}
+	}
+}
