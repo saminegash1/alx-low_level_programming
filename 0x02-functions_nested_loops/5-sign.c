18379683116dc5d197a0189375c35f195fa42c2b
Samuel N <samuelnegash56@gmail.com>
2022-06-07 20:42:41 +0300

    return sign depending on condition

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_sign -> prints sign based on condition
+* @n: argument passed
+* Return: 1, 0 -1
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
