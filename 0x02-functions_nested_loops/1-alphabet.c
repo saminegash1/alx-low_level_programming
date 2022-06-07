df3d00848dc1d90eeb4940acc8ad7e62068e26ec
Samuel N <samuelnegash56@gmail.com>
2022-06-07 20:19:22 +0300

    added a function that prints an alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_alphabet -> prints the lowercase alphabets
+*/
+
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
