760cee063b3f329316e14ab6298df81465f752fe
Samuel N <samuelnegash56@gmail.com>
2022-06-07 20:47:08 +0300

    absoluter value done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _abs -> return absolute value of a number
+* @n: parameter
+* Return: absolute value of a number
+*/
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
+
