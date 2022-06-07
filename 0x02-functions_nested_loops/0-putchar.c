1c0517eebd4fb22177b85787a88c415e365ad396
Samuel N <samuelnegash56@gmail.com>
2022-06-07 20:03:18 +0300

    putchar printed

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/** main - prints putchar 
+* Return: always 0
+*/
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+	
+	return (0);
+	
+}
