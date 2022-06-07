52766069c19f5a55d6fccac76440f8a6fe1da2b8
Samuel N <samuelnegash56@gmail.com>
2022-06-07 21:45:33 +0300

    printing to 98 from both direction

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ void print_to_98(int n)
 				printf("%d\n", n);
 			else
 				printf("%d, ", n);
-			n = n++;
+			n = n + 1;
 		}
 	} else if (n > 98)
 	{
@@ -26,6 +26,7 @@ void print_to_98(int n)
 				printf("%d\n", n);
 			else
 				printf("%d, ", n);
+			n = n - 1;
 		}
 	}
 }
