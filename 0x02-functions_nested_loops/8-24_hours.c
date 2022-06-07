f4fcd2fb28aed2a26a4fb6965694db55ba945b9c
Samuel N <samuelnegash56@gmail.com>
2022-06-07 21:06:15 +0300

    24 hours printing

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+* jack_bauer -> prints 24 hours
+*/
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			if (i < 10)
+			{
+				_putchar('0');
+				_putchar(i + '0');
+			}
+			else if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			if (j < 10)
+			{
+				_putchar(':');
+				_putchar('0');
+				_putchar(j + '0');
+			}
+			else if (j > 10)
+			{
+				_putchar(':');
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+		}
+	}
+}
