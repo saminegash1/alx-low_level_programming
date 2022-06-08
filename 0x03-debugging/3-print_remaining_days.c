3e942b74b64a9c644cd9a794a8ab6a4928c88423
Samuel N <samuelnegash56@gmail.com>
2022-06-08 23:58:13 +0300

    another commit

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,10 +13,9 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 && year % 100) || (!(year % 100) && !(year %
-	400)))
+	if ((year % 4 == 0) && (year % 400 == 0 || year % 100 != 0))
 	{
-		if (month > 2)
+		if (month >= 3 && day >= 60)
 		{
 			day++;
 		}
@@ -28,8 +27,7 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day -
-			31, year);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int);
+void positive_or_negative(int i);
 int largest_number(int, int, int);
 void print_remaining_days(int, int, int);
 
