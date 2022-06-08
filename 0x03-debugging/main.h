959c9964af93e1957110718cdd26ec6cf56279bf
Samuel N <samuelnegash56@gmail.com>
2022-06-08 23:54:01 +0300

    leap years

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,8 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
+void positive_or_negative(int);
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
 
 #endif /* MAIN_H */
