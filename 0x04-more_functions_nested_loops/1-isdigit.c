79725471cb6a6cde18119b91d7d08c78f40d7d28
Samuel N <samuelnegash56@gmail.com>
2022-06-09 19:36:26 +0300

    is digit done

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* _isdigit - funtion to check for a digit
+* @c: an integer argument
+* Return: success
+*/
+int _isdigit(int c)
+{
+	return (c <= '0' && c >= '9');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 int _putchar(char c);
 int _isupper(int c);
+int _isdigit(int c);
 
 #endif
