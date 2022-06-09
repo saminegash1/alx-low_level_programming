b52a8c96b60d2b844a42cfd4e28643bde9643639
Samuel N <samuelnegash56@gmail.com>
2022-06-09 19:32:44 +0300

    checking upper case letter

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* _isupper -> this is a function to check upper case alphas
+* @c: an integer paramer
+* Return: something but success
+*/
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
+int _isupper(int c);
 
 #endif
