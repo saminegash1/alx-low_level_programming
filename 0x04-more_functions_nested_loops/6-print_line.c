2ebcc0e9bbd31aabf1630882c195798a472f5052
Samuel N <samuelnegash56@gmail.com>
2022-06-09 20:22:10 +0300

    hot fix

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,8 +10,10 @@ void print_line(int n)
 
 	if (n <= 0)
 		_putchar('\n');
-		return;
-	for (x = 0; x < n; x++)
-		_putchar(95);
-	_putchar('\n');
+	else
+	{
+		for (x = 0; x < n; x++)
+			_putchar(95);
+		_putchar('\n');
+	}
 }
