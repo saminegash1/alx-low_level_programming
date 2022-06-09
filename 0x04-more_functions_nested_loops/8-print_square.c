c2b9e5833a60a0417c35142aab0d2c91ff5dc4f3
Samuel N <samuelnegash56@gmail.com>
2022-06-09 20:44:01 +0300

    print squere

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_square -> printing line
+* @size: integer params
+*/
+void print_square(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y< size: y++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
