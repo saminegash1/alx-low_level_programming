41fdbc7926740231b9f45a5463584dcfab8ee122
Samuel N <samuelnegash56@gmail.com>
2022-06-09 19:48:10 +0300

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_most_numbers -> this prints most numbers
+*/
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		if (x !=2 && x !=4)
+			_putchar(x + '0');
+	}
+	_putchar('\n');
+}
