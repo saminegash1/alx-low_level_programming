33ff714d456cb6a963fc1231860d4cec2c99182e
Samuel N <samuelnegash56@gmail.com>
2022-06-09 19:56:26 +0300

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* more_numbers -> printing more numbers
+*/
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 15; y++)
+		{
+			_putchar(y + '0');
+		}
+		_putchar('\n');
+	}	
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,5 +6,6 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
 
 #endif
