49fa97f79442b2342f7660b7dd620c76552c7b8b
Samuel N <samuelnegash56@gmail.com>
2022-06-13 22:15:58 +0300

    revering string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* rev_string -> reversing a string
+* @s: parameter s
+*/
+void rev_string(char *s)
+{
+	char tmp;
+	int i, length1, length2;
+
+	length1 = 0;
+	length2 = 0;
+
+	while (s[length1] != '\0')
+		length1++;
+
+	length2 = length1 - 1
+	for (i = 0; i < length1 / 2; i++)
+	{
+		"samalx" "xlamas"
+		tmp = s[i];
+		s[i] = s[length2];
+		s[length2] = tmp;
+		length2 -= 1;
+		
+	}
+}
