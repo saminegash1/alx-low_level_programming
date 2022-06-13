0cb7b59f3151f3e276f5154929b12efb14ab184b
Samuel N <samuelnegash56@gmail.com>
2022-06-13 22:01:48 +0300

    new fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,19 @@
 #include "main.h"
-#include <string.h>
 
 /**
 * print_rev -> printing a string in reverse
 * @s: the string to be printed in rev
 */
+
 void print_rev(char *s)
 {
 	int i, n;
 
-	n = strlen(s);
-	for (i = n - 1; i >= 0; i--)
+	n = 0;
+	while (s[n] != '\0')
+		n++
+
+	for (i = n-1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
-void print_rev(char *s)
+void print_rev(char *s);
+
 #endif
