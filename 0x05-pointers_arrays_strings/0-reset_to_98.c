e737ef918b8a00192b1294c426e3f0d2c7b97664
Samuel N <samuelnegash56@gmail.com>
2022-06-13 21:12:50 +0300

    reseting using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+* reset_to_98 -> a function to update the reference of a pointer
+* @n: pointer to n to be updated
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+## Pointers, Arrays and Strings
+**98 Battery st:** Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.
