0da6105618e63754d62999f2b599f41d4b546d83
Samuel N <samuelnegash56@gmail.com>
2022-06-13 22:22:08 +0300

    puts2 funciton

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* puts2 -> puts2 function 
+* @str: str parameter
+*/
+void puts2(char *str)
+{
+	int len;
+
+	len = strlen(str);
+	for (i = 0; i < len; i ++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,4 +6,5 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void rev_string(char *s);
+void puts2(char *str);
 #endif
