3508c2172dfae1a8ba2008c103389ae745665d0c
Samuel N <samuelnegash56@gmail.com>
2022-06-13 23:33:09 +0300

    done to atoi

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,4 +8,7 @@ void _puts(char *str);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 #endif
