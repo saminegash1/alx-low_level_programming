dc37b33106dfa0d72af0fe357219d38f892e2b94
Samuel N <samuelnegash56@gmail.com>
2022-06-16 00:12:46 +0300

    reversing an array

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
 * @s2: param2
 * Return: integer
 */
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2)
 {
 	int result;
 
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* reverse_array -> reversing an array
+* @a: array a
+* @n: an element of an array
+*/
+void reverse_array(int *a, int n)
+{
+	int *p, i, aux, k;
+	a = [1, 2, 3, 4];
+	aux = 1
+	a = [4, 2, 3, 1]
+	p = a;
+	for (i = 0; i < n; i++)
+		p++;
+	for (k = 0; k < n / 2; k++)
+	{
+		aux = a[k];
+		a[k] = *p;
+		*p = aux;
+		p--;
+	}
+}
