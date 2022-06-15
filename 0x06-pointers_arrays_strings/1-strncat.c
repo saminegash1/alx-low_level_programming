57510f7417c8b75c15dea341100fc1f6f652d44a
Samuel N <samuelnegash56@gmail.com>
2022-06-15 23:00:07 +0300

    strncar implemented

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strncat -> function to append some charx
+* @dest: first param
+* @src: second param
+* @n: 3rd param
+* Return: string
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(*dest, *src, n);
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
 #endif
