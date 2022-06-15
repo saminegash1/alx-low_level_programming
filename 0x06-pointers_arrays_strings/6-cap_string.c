ce5daf12a1d09fd16854459ab5ff86a241a2b4e0
Samuel N <samuelnegash56@gmail.com>
2022-06-16 00:42:16 +0300

    capitalization completed

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* cap_string -> capitalization function
+* @x: string param
+* Return: capitalized version of the string
+*/
+char *cap_string(char *x)
+{	x = "sami,is;in learn programming africa"
+	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int len = 13;
+	int a = 0, i;
+
+	while (x[a])
+	{
+		i = 0;
+		while (i < len)
+		{
+			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+				s[a] = s[a] - 32;
+			i++;
+		}
+		a++;
+	}
+	return (x);
+}
