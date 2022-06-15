96b788fa7270421616beeb37aba509f14f9b467e
Samuel N <samuelnegash56@gmail.com>
2022-06-15 23:13:28 +0300

    strcmp implemented

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strcmp -> for comparing purpose
+* @s1: param1
+* @s2: param2
+* Return: integer
+*/
+int _strcmp(char *s1, char *s2);
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
