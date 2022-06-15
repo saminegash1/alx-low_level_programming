9f74a482dca490ecec123a2ac66e6036bcef720f
Samuel N <samuelnegash56@gmail.com>
2022-06-15 23:07:46 +0300

    strncpy is implemented

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strncpy -> for copying purpose
+* @dest: param1
+* @src: param2
+* @n: param3
+* Return: string
+*/
+char *_strncpy(char *dest, char *src, int n)\
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
