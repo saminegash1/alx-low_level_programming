69517b1438dd92a48cd96ed6cfb9743b237f6afb
Samuel N <samuelnegash56@gmail.com>
2022-06-22 21:52:54 +0300

    the first recursion solved

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _puts_recursion -> this is a function to print string recursivly
+* @s: string paramenter
+*/
+void _puts_recursion(char *s)
+{
+	s = "lpa\0"
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
