336d5a386457d809afedd91cb61eba9cc4699d6c
Samuel N <samuelnegash56@gmail.com>
2022-06-22 22:28:20 +0300

    power function in recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*	_pow_recursion -> power using recursion
+* @x: x ^ y
+* @y: the power number
+* Return: power result
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recurstion(x, y - 1));
+}
