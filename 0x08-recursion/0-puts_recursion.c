57142469d95a4d9d6752785df7e27f3acbd18455
Samuel N <samuelnegash56@gmail.com>
2022-06-22 21:54:32 +0300

    hot fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,7 +6,6 @@
 */
 void _puts_recursion(char *s)
 {
-	s = "lpa\0"
 	if (*s == '\0')
 	{
 		_putchar('\n');
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
+void _puts_recursion(char *s);
 
 
 #endif
