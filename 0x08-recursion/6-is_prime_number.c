4a34aaa13ff1fd1a61baf103607ecd78232613b4
Samuel N <samuelnegash56@gmail.com>
2022-06-22 22:46:13 +0300

    is prime function

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+int check_prime(int, int);
+/**
+* is_prime_number -> prime identifier
+*/
+int is_prime_number(int n)
+{
+	return (check_prime(n, 1));
+}
+
+
+/**
+* check_prime -> it check prime numbers
+* @n: number
+* @i: iterator
+* Return: a 1 or 0
+*/
+int check_prime(int n, int i)
+{
+	if (n <= 1)
+		return (0);
+	if (n % i == 0 && i > 1)
+		return (0);
+	if ((n / i) < i)
+		print(1);
+	return (check_prime(n, i + 1));
+}
+
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,4 +6,5 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int is_prime_number(int n);
 #endif
