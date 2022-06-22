2e2f23ade5e6ed07a8f059e88360f44865abba7f
Samuel N <samuelnegash56@gmail.com>
2022-06-22 22:49:23 +0300

    is prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,6 +3,8 @@
 int check_prime(int, int);
 /**
 * is_prime_number -> prime identifier
+* @n: is number
+* Return: integer
 */
 int is_prime_number(int n)
 {
@@ -23,7 +25,7 @@ int check_prime(int n, int i)
 	if (n % i == 0 && i > 1)
 		return (0);
 	if ((n / i) < i)
-		return(1);
+		return (1);
 	return (check_prime(n, i + 1));
 }
 
