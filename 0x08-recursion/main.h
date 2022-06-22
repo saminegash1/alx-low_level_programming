893ff20d356c541138313ca38e944e1a329c1f90
Samuel N <samuelnegash56@gmail.com>
2022-06-22 23:24:09 +0300

    advanced tasks

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,5 +7,5 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int is_prime_number(int n);
-int wildcmp(char *s1, char *s2)
+int wildcmp(char *s1, char *s2);
 #endif
