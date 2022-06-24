b925acc83ae10afd2a9e5b38c102a8cf9e30d383
Samuel N <samuelnegash56@gmail.com>
2022-06-24 22:11:24 +0300

    printing the name of the file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main -> this is a function to print its name
+* @argc: argc parameter
+* @argv: an array of a command listed
+* Return: 0 for success
+*/
+int main (int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+# this is all about argc and argv
