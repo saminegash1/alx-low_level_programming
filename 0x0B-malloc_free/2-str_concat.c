75511bec4e653c077320360dd2d2c7854f3aeace
Samuel N <samuelnegash56@gmail.com>
2022-06-28 02:55:34 +0300

    str concatination done

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+* str_concat -> string concatinating function
+* @s1: string 1
+* @s2: string 2
+* Return: string 1 + string 2
+*/
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0, l = 0, k = 0;
+	char *s;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i])
+		i++;
+	while (s2[j])
+		j++;
+
+	l = i + j;
+	s = (char *)malloc(l * sizeof(char) + 1);
+	j = 0;
+	while (k < l)
+	{
+		if (k <= i)
+			s[k] = s1[k];
+		if (k > i)
+		{
+			s[k] = s2[j];
+			j++
+		}
+		k++;
+	}
+	s[k] = '\0';
+	return (s);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,4 +4,5 @@
 #include <stdlib.h>
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
+char *str_concat(char *s1, char *s2);
 #endif
