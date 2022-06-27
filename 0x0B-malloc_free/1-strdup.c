aa1b58ea31fc7060d31e9ff46e1ef3e1fa792327
Samuel N <samuelnegash56@gmail.com>
2022-06-28 02:44:24 +0300

    strdup internal function duplication

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _strdup -> string duplicator function
+* @str: string to be dupliated
+* Return: a string pointer
+*/
+char *_strdup(char *str)
+{
+	int i = 1, j = 0;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+	while str[i]
+		i++;
+	s = (char *)malloc(i * sizeof(char) + 1);
+	if (s == NULL)
+		return (NULL);
+	while (j < i)
+	{
+		s[j] = str[j];
+		j++;
+	}
+	s[j] = '\0';
+	return (s);
+}
