83106a94c32973fb08e445450b8e1ebef2e619f8
Samuel N <samuelnegash56@gmail.com>
2022-06-28 02:30:04 +0300

    hot fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
 * create_array -> creating array
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stddef.h>
+#include <stdlib.h>
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 #endif
