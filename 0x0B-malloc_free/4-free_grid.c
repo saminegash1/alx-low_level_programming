0439a7412a2e40eafd7baa31657fc26a0afba920
Samuel N <samuelnegash56@gmail.com>
2022-06-28 03:28:11 +0300

    hot fix

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* free_grid -> removing memory allocation
+* @grid: grid to be freed
+* @height: height of matrix
+* Return: nothing
+*/
+void free_grid(int **grid, int height);
+{
+	int i;
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
