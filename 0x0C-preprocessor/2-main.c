b8d242220790e4ea34e953df6b5c3a09bc108f20
Samuel Negash <samuelnegash56@gmail.com>
2022-07-12 22:43:44 +0300

    preprocessor completed

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file of the program
+ *        was compiled from, followed by a new line.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
